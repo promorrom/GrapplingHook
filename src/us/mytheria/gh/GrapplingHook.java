e9047cdb44b5ccf15f6e4af09c51391cb1720b6b
promorrom <lubensaico@gmail.com>
2021-01-04 08:57:25 -0500

    1.8 Fishhook Velocity

--- src/us/mytheria/gh/GrapplingHook.java
+++ src/us/mytheria/gh/GrapplingHook.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 
 import org.bukkit.Bukkit;
@@ -16,7 +17,6 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.FishHook;
-import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -32,12 +32,16 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
 
 public class GrapplingHook extends JavaPlugin implements Listener, CommandExecutor {
 
 	ItemStack gH;
 	NamespacedKey hK = new NamespacedKey(this, "hookKey");
+
+	private Random random;
+	private boolean hasDifferentGravity;
 
 	public HashMap<Integer, Integer> noFallEntities = new HashMap<Integer, Integer>();
 
@@ -53,12 +57,26 @@ public class GrapplingHook extends JavaPlugin implements Listener, CommandExecut
 	String badRod = "Messages.WrongModelData";
 	String cM = "Messages.Cooldown";
 
+	Boolean statTrak = true;
+	Boolean doFallDamage = true;
+	Boolean updateItemMeta = true;
+
 	/* Defaults */
 	String[] grapplingLore = { ChatColor.GRAY + "Travel around in style using", ChatColor.GRAY + "this Grappling Hook",
 			ChatColor.DARK_GRAY + "2 Second Cooldown", "",
 			ChatColor.GRAY + "Total uses: " + ChatColor.WHITE + "%totalUses%", "",
 			ChatColor.GREEN.toString() + ChatColor.BOLD + "UNCOMMON" };
 	HashMap<String, Long> cooldown = new HashMap<>();
+
+	String a = getServer().getClass().getPackage().getName();
+	String version = a.substring(a.lastIndexOf('.') + 1);
+
+	public void imBadWithNames() {
+		statTrak = getConfig().getBoolean(statTrack);
+		doFallDamage = getConfig().getBoolean(fallDamage);
+		updateItemMeta = getConfig().getBoolean(badRodBoolean);
+		random = new Random();
+	}
 
 	public void loadConfiguration() {
 		getConfig().addDefault(dN, ChatColor.GREEN + "Grappling Hook");
@@ -92,6 +110,19 @@ public class GrapplingHook extends JavaPlugin implements Listener, CommandExecut
 		is.setItemMeta(im);
 	}
 
+	/*
+	 * public void applyItemMeta18(ItemStack is) { is = NBTEditor.set(is, 0, "test",
+	 * "GrapplingHook", "totalUses"); is = NBTEditor.set(is, getConfig().getInt(mD),
+	 * "test", "GrapplingHook", "CustomModelData"); ItemMeta im = is.getItemMeta();
+	 * im.setDisplayName(getConfig().getString(dN));
+	 * im.setLore(getConfig().getStringList(mL)); int stat = NBTEditor.getInt(is,
+	 * "test", "GrapplingHook", "totalUses"); List<String> desc = im.getLore(); desc
+	 * = desc.stream().map(s -> s.replace("%totalUses%",
+	 * String.valueOf(stat))).collect(Collectors.toList()); im.setLore(desc);
+	 * im.spigot().setUnbreakable(true); im.addItemFlags(ItemFlag.HIDE_UNBREAKABLE);
+	 * is.setItemMeta(im); }
+	 */
+
 	public void countStat(ItemStack i, ItemMeta im) {
 		int stat = im.getPersistentDataContainer().get(hK, PersistentDataType.INTEGER) + 1;
 		im.getPersistentDataContainer().set(hK, PersistentDataType.INTEGER, stat);
@@ -109,7 +140,7 @@ public class GrapplingHook extends JavaPlugin implements Listener, CommandExecut
 				if ((cooldown.get(p.getName()) + getConfig().getLong(coolDown)) < (System.currentTimeMillis() / 1000)) {
 					cooldown.put(e.getPlayer().getName(), (System.currentTimeMillis() / 1000));
 					pullEntityToLocation(p, loc);
-					if (getConfig().getBoolean(statTrack) == true) {
+					if (statTrak == true) {
 						countStat(i, im);
 					}
 				} else {
@@ -118,14 +149,14 @@ public class GrapplingHook extends JavaPlugin implements Listener, CommandExecut
 			} else {
 				cooldown.put(e.getPlayer().getName(), (System.currentTimeMillis() / 1000));
 				pullEntityToLocation(p, loc);
-				if (getConfig().getBoolean(statTrack) == true) {
+				if (statTrak == true) {
 					countStat(i, im);
 				}
 			}
 		} else {
 			Location loc = e.getHook().getLocation();
 			pullEntityToLocation(p, loc);
-			if (getConfig().getBoolean(statTrack) == true) {
+			if (statTrak == true) {
 				countStat(i, im);
 			}
 		}
@@ -134,10 +165,63 @@ public class GrapplingHook extends JavaPlugin implements Listener, CommandExecut
 	public void onEnable() {
 		loadConfiguration();
 		Bukkit.getPluginManager().registerEvents(this, this);
+		random = new Random();
 
 		gH = new ItemStack(Material.FISHING_ROD);
 		applyItemMeta(gH);
 
+	}
+
+	@EventHandler
+	public void onFishEvent(PlayerFishEvent event) {
+		if (event.getState() != PlayerFishEvent.State.FISHING)
+			return;
+		FishHook fishHook = event.getHook();
+
+		final Location location = event.getPlayer().getLocation();
+		final double playerYaw = location.getYaw();
+		final double playerPitch = location.getPitch();
+
+		final float oldMaxVelocity = 0.4F;
+		double velocityX = -Math.sin(playerYaw / 180.0F * (float) Math.PI)
+				* Math.cos(playerPitch / 180.0F * (float) Math.PI) * oldMaxVelocity;
+		double velocityZ = Math.cos(playerYaw / 180.0F * (float) Math.PI)
+				* Math.cos(playerPitch / 180.0F * (float) Math.PI) * oldMaxVelocity;
+		double velocityY = -Math.sin(playerPitch / 180.0F * (float) Math.PI) * oldMaxVelocity;
+
+		final double oldVelocityMultiplier = 1.5;
+
+		final double vectorLength = (float) Math
+				.sqrt(velocityX * velocityX + velocityY * velocityY + velocityZ * velocityZ);
+		velocityX /= vectorLength;
+		velocityY /= vectorLength;
+		velocityZ /= vectorLength;
+
+		velocityX += random.nextGaussian() * 0.007499999832361937D;
+		velocityY += random.nextGaussian() * 0.007499999832361937D;
+		velocityZ += random.nextGaussian() * 0.007499999832361937D;
+
+		velocityX *= oldVelocityMultiplier;
+		velocityY *= oldVelocityMultiplier;
+		velocityZ *= oldVelocityMultiplier;
+
+		fishHook.setVelocity(new Vector(velocityX, velocityY, velocityZ));
+
+		if (!hasDifferentGravity)
+			return;
+		new BukkitRunnable() {
+			@Override
+			public void run() {
+				if (!fishHook.isValid() || fishHook.isOnGround())
+					cancel();
+				if (!fishHook.isInWater()
+						&& fishHook.getWorld().getBlockAt(fishHook.getLocation()).getType() != Material.WATER) {
+					final Vector fVelocity = fishHook.getVelocity();
+					fVelocity.setY(fVelocity.getY() - 0.01);
+					fishHook.setVelocity(fVelocity);
+				}
+			}
+		}.runTaskTimer(this, 1, 1);
 	}
 
 	public void renameLater(PlayerFishEvent e, ItemStack i) {
@@ -148,23 +232,21 @@ public class GrapplingHook extends JavaPlugin implements Listener, CommandExecut
 				if (im.getCustomModelData() == getConfig().getInt(mD)) {
 					if (e.getState() == State.FISHING) {
 						FishHook h = e.getHook();
-						h.setMaxWaitTime(20 * 199);
-						h.setMinWaitTime(20 * 99);
-						h.setVelocity(new Vector(h.getVelocity().getX(), h.getVelocity().getY() * 0.80,
-								h.getVelocity().getZ()));
+						h.setMaxWaitTime(20 * 5);
+						h.setMinWaitTime(20 * 2);
+						h.getPersistentDataContainer().set(hK, PersistentDataType.STRING, "on");
 					}
 					if (e.getState() == State.REEL_IN || e.getState() == State.IN_GROUND) {
 						doGrapple(p, e, i, im);
 
 					}
 					if (e.getState() == State.CAUGHT_FISH) {
-						ItemStack fished = ((Item) e.getCaught()).getItemStack();
-						fished.setAmount(0);
-						e.setExpToDrop(0);
+						e.setCancelled(true);
+						e.getHook().remove();
 						doGrapple(p, e, i, im);
 					}
 				} else {
-					if (getConfig().getBoolean(badRodBoolean) == true) {
+					if (updateItemMeta == true) {
 						ItemStack h = i;
 						im.setCustomModelData(getConfig().getInt(mD));
 						h.setItemMeta(im);
@@ -181,7 +263,7 @@ public class GrapplingHook extends JavaPlugin implements Listener, CommandExecut
 			ItemMeta im = i.getItemMeta();
 			if (im.getPersistentDataContainer().has(hK, PersistentDataType.INTEGER)) {
 				if (im.getCustomModelData() != getConfig().getInt(mD)) {
-					if (getConfig().getBoolean(badRodBoolean) == true) {
+					if (updateItemMeta == true) {
 						int stat = im.getPersistentDataContainer().get(hK, PersistentDataType.INTEGER);
 						im.setDisplayName(getConfig().getString(dN));
 						im.setCustomModelData(getConfig().getInt(mD));
@@ -255,9 +337,9 @@ public class GrapplingHook extends JavaPlugin implements Listener, CommandExecut
 		double v_z = (1.0 + 0.07 * t) * (loc.getZ() - entityLoc.getZ()) / t;
 
 		Vector v = e.getVelocity();
-		v.setX(v_x * 3.75);
+		v.setX(v_x * 2.35);
 		v.setY(v_y);
-		v.setZ(v_z * 3.75);
+		v.setZ(v_z * 2.35);
 		e.setVelocity(v);
 
 		addNoFall(e, 100);
@@ -280,7 +362,7 @@ public class GrapplingHook extends JavaPlugin implements Listener, CommandExecut
 
 	@EventHandler(priority = EventPriority.HIGHEST)
 	public void onFallDamage(EntityDamageEvent e) {
-		if (e.getCause() == DamageCause.FALL && getConfig().getBoolean(fallDamage) == true) {
+		if (e.getCause() == DamageCause.FALL && doFallDamage == true) {
 			if (noFallEntities.containsKey(e.getEntity().getEntityId())) {
 				e.setCancelled(true);
 			}
@@ -298,6 +380,7 @@ public class GrapplingHook extends JavaPlugin implements Listener, CommandExecut
 					case "reload":
 						reloadConfig();
 						applyItemMeta(gH);
+						imBadWithNames();
 						s.sendMessage("Config reloaded");
 						break;
 					case "get":
